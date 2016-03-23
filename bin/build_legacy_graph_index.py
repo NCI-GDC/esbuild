1c306217e2f00f5db8f566ad9d227bfce1d259a2
Joshua Miller <jsmiller@uchicago.edu>
2016-03-23 05:54:40 -0500

    chore(bin): update legacy index_base

--- bin/build_legacy_graph_index.py
+++ bin/build_legacy_graph_index.py
@@ -13,7 +13,10 @@ def main():
         help='if passed, do not roll the alias and delete old indices')
 
     args = parser.parse_args()
-    gdc_es = GDCElasticsearch(converter_class=LegacyGraphIndexBuilder)
+    gdc_es = GDCElasticsearch(
+        converter_class=LegacyGraphIndexBuilder,
+        index_base="gdc_legacy_graph",
+    )
     gdc_es.go(roll_alias=not args.no_roll)
 
 if __name__ == "__main__":
