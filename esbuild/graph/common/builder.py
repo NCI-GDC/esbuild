ae166de6c2a0248114f351c5339c138b7e078ce1
Joshua Miller <jsmiller@uchicago.edu>
2016-05-17 08:46:35 -0500

    chore/skip-selected-target-projects

--- esbuild/graph/common/builder.py
+++ esbuild/graph/common/builder.py
@@ -222,6 +222,9 @@ class GraphIndexBuilder(object):
             ('TCGA', 'FPPP'),
             ('GDC', 'INTERNAL'),
             ('UAT08', 'BROAD-BCR'),
+            ('TARGET', 'AML-IF'),
+            ('TARGET', 'ALL-P1'),
+            ('TARGET', 'ALL-P2'),
         }
 
         # The body of these nested documents will be flattened into
