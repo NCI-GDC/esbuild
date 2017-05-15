41aa9b666697a2c862e3d6553616b37512105ee8
Joe Sislow <madopal@uchicago.edu>
2017-05-15 19:41:46 +0000

    feat(esbuild): Add delete flag, write file, add args for testing
    - Add flag to manually trigger delete of to_delete files
    - Add dump of list of UUIDs that are ok to delete
    - Add flag to allow testing of delete flags

--- bin/build_active_graph_index.py
+++ bin/build_active_graph_index.py
@@ -11,10 +11,31 @@ def main():
     parser.add_argument(
         '--no-roll', action="store_true",
         help='if passed, do not roll the alias and delete old indices')
+    parser.add_argument(
+        '--delete', action="store_true",
+        help='if passed, delete the nodes marked "to_delete" after running, unless --json_delete is passed')
+    parser.add_argument(
+        '--json_delete',
+        help='file to use to delete nodes and skip running esbuild')
 
     args = parser.parse_args()
     gdc_es = GDCElasticsearch(converter_class=ActiveGraphIndexBuilder)
-    gdc_es.go(roll_alias=not args.no_roll)
+    if args.delete:
+        if not args.json_delete:
+            gdc_es.go(roll_alias=not args.no_roll,
+                      delete_nodes=args.delete,
+                      skip_build=args.test_delete)
+        else:
+            nodes_to_delete=[]
+            with open(args.json_delete, 'r') as in_file:
+                for line in in_file:
+                    nodes_to_delete.append(line.strip('\n'))
+            gdc_es.delete_nodes(to_delete=nodes_to_delete,
+                          delete_nodes=args.delete)
+    else:
+        gdc_es.go(roll_alias=not args.no_roll,
+                  delete_nodes=args.delete,
+                  skip_build=args.test_delete)
 
 if __name__ == "__main__":
     main()
