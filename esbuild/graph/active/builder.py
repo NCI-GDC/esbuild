93c46a77c0da4c8ac5f0047678f446cbbb610717
Max Popov <maxpmy@gmail.com>
2017-08-07 11:17:08 -0500

    Omitted CCLE-CCLE_V2

--- esbuild/graph/active/builder.py
+++ esbuild/graph/active/builder.py
@@ -219,6 +219,7 @@ class ActiveGraphIndexBuilder(GraphIndexBuilder):
         super(ActiveGraphIndexBuilder, self).__init__(*args, **kwargs)
 
         # Omit entities from these projects
+        self.omitted_projects.add(('CCLE', 'CCLE_V2'))
         self.omitted_projects.add(('TARGET', 'ALL-P1'))
         self.omitted_projects.add(('TARGET', 'ALL-P2'))
         self.omitted_projects.add(('CCLE', 'ALL-P1'))
