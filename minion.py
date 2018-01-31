50cdc9ebdd2e7a40618b76c252a96434a7667718
Max Popov <maxpmy@gmail.com>
2018-01-31 11:31:53 -0600

    feat(runner): Single machine multiple runs mode

--- minion.py
+++ minion.py
@@ -3,6 +3,7 @@ import argparse
 import time
 import yaml
 import os
+import subprocess
 
 from master import esbuild_argparser
 from cdisutils.log import get_logger
@@ -28,7 +29,9 @@ def minion_argparser():
     parser.add_argument('--queue-id', type=str,
                         help='Depot queue id to listen to. Has to be UUID string',
                         required=True)
-
+    parser.add_argument('--do-not-wait-for-completion', action='store_true',
+                        help='If set, will not wait for esbuild completion. '
+                        'Will result in all jobs in the queue being run on the machine')
     return parser
 
 
@@ -51,11 +54,15 @@ if __name__ == "__main__":
             # Make sure that arguments are valid:
             esbuild_argparser().parse_args(work['arguments'])
             # Compose and execute the command:
-            command = ('sudo /var/tungsten/services/esbuild/es_build_{}_wrapper {}'
-                       .format(work['build_type'], ' '.join(work['arguments'])))
-            logger.info('-> Running {}'.format(command))
-            os.system(command)
+            command = ['sudo /var/tungsten/services/esbuild/es_build_{}_wrapper'
+                       .format(work['build_type'])] + work['arguments']
+            logger.info('-> Running {}'.format(' '.join(command)))
+            if args.do_not_wait_for_completion:
+                # NOTE: Will result in all jobs of the queue running on a single machine
+                subprocess.Popen(command, shell=True)
+            else:
+                subprocess.call(command, shell=True)
         except Exception as err:
-            logger.error("Attempted to run job: {}\nError: {}".format(work, err))
+            logger.error("Attempted to run job: {}\nError: {}".format(work, repr(err)))
 
         time.sleep(TIMEDELTA)
