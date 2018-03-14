97712e9371e90ae8a7154211060cef1399825189
Joe Sislow <madopal@uchicago.edu>
2018-03-14 18:32:00 +0000

    fix(mapping): Singularize slide_image label
    - Singularize slide_image label

--- esbuild/graph/common/mappings.py
+++ esbuild/graph/common/mappings.py
@@ -131,7 +131,7 @@ class ESMapper(object):
         case_tree.sample.portion.corr = (ONE_TO_MANY, 'portions')
         case_tree.sample.slide.corr = (ONE_TO_MANY, 'slides')
         case_tree.sample.slide.annotation.corr = (ONE_TO_MANY, 'annotations')
-        case_tree.sample.slide.slide_image.corr = (ONE_TO_ONE, 'slide_images')
+        case_tree.sample.slide.slide_image.corr = (ONE_TO_ONE, 'slide_image')
         case_tree.sample.portion.analyte.corr = (ONE_TO_MANY, 'analytes')
         case_tree.sample.portion.analyte.annotation.corr = (ONE_TO_MANY, 'annotations')
         case_tree.sample.portion.analyte.aliquot.corr = (ONE_TO_MANY, 'aliquots')
