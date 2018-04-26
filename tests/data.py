525254da9ec681a85a9d2f6ded8d37fc8f85ceb3
Justin Barnowski <jbarno@uchicago.edu>
2018-04-26 11:47:31 -0500

    fix(data): fixing static test data from horton

--- tests/data.py
+++ tests/data.py
@@ -399,7 +399,7 @@ NODES = [
     Treatment(
         node_id='4768cc70-ca97-4af8-9e66-6947c75a9376',
         state='submitted',
-        days_to_treatment=None,
+        days_to_treatment_end=None,
         project_id=u'TCGA-DEV3',
         submitter_id=u'TCGA-DEV-3-CASE-014-DIAG1-TR1',
         therapeutic_agents=None,
