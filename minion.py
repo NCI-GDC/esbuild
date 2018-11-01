d08bc4dad39f8e60258edb35325e86370a97199c
Max Popov <maxpmy@gmail.com>
2018-11-01 16:04:02 -0500

    chore(statsd): master and minion events

--- minion.py
+++ minion.py
@@ -1,9 +1,9 @@
 import requests
-import argparse
 import time
 import yaml
 import os
 import subprocess
+from datadog import statsd
 
 from parsers import (
     Parser,
@@ -28,37 +28,48 @@ def minion_argparser():
     return Parser.build_parser([
         DepotArgs,
         MinionArgs,
+        EsbuildUserArgs,
+        EsbuildPrivateArgs,
     ], description='Esbuild minion arguments parser')
 
 
-def get_work(args):
+def get_job(args):
     """
-    Get work from depot queue
+    Get job from depot queue
     """
-    # Get work from depot api:
-    work = requests.get('http://{}:{}/v0/work/{}'
-                        .format(args.host, args.port, args.queue_id))
+    # Get job from depot api:
+    job = requests.get('http://{}:{}/v0/work/{}'
+                       .format(args.host, args.port, args.queue_id))
     try:
-        work = work.json()
+        job = job.json()
+        esbuild_args = job['esbuild_args']
     except Exception as err:
-        logger.error("Invalid job: {}\nError: {}".format(work, err))
-
-    if work.get('status') == 'No work found':
+        logger.error("Invalid job: {}\nError: {}".format(job, err))
         return
 
+    if job.get('status') == 'No job found':
+        return
+
+    statsd.event(
+        "Job received",
+        "job: {}".format(job),
+        source_type_name="esbuild-minion",
+        alert_type="info",
+        tags=["es_index:{}".format(args.index_name), 'minion'],
+    )
     # Make sure that arguments are valid:
     esbuild_parser = Parser.build_parser([EsbuildUserArgs, EsbuildPrivateArgs])
-    esbuild_parser.parse_args(work['esbuild_args'])
-    return work
+    esbuild_parser.parse_args(esbuild_args)
+    return job
 
 
-def get_minion_command(work):
+def get_minion_command(job):
     """
-    Prepares the command for minion to run given the work json
+    Prepares the command for minion to run given the job json
     """
-    esbuild_args = work['esbuild_args']
+    esbuild_args = job['esbuild_args']
     command = ['sudo /var/tungsten/services/esbuild/es_build_{}_wrapper'
-               .format(work['index_type'])] + esbuild_args
+               .format(job['index_type'])] + esbuild_args
     command = ' '.join(map(str, command))
     return command
 
@@ -66,10 +77,10 @@ def get_minion_command(work):
 if __name__ == "__main__":
     args = minion_argparser().parse_args()
     while True:
-        work = get_work(args)
-        if work:
+        job_json = get_job(args)
+        if job_json:
             # Compose and execute the command:
-            command = get_minion_command(work)
+            command = get_minion_command(job_json)
             logger.info('-> Running {}'.format(command))
             if args.do_not_wait_for_completion:
                 # NOTE: Will result in all jobs of the queue running on a single machine
