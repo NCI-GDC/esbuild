4acc4b6275bf2aea69b68b371b09248dde4f2b01
Joe Sislow <madopal@uchicago.edu>
2018-11-20 19:00:38 +0000

    feat(minion): Make minion multithreaded, adding args to control how many threads

--- minion.py
+++ minion.py
@@ -4,11 +4,13 @@ import time
 import yaml
 import os
 import subprocess
+from multiprocessing import Process
+import multiprocessing
 
 from master import esbuild_argparser
 from cdisutils.log import get_logger
-logger = get_logger('esbuild_minion')
 
+logger = get_logger('esbuild_minion')
 root_dir = os.path.dirname(os.path.abspath(__file__))
 config = yaml.safe_load(open(os.path.join(root_dir, 'config.yml'), 'r').read())
 
@@ -29,40 +31,68 @@ def minion_argparser():
     parser.add_argument('--queue-id', type=str,
                         help='Depot queue id to listen to. Has to be UUID string',
                         required=True)
-    parser.add_argument('--do-not-wait-for-completion', action='store_true',
-                        help='If set, will not wait for esbuild completion. '
-                        'Will result in all jobs in the queue being run on the machine')
+    parser.add_argument('--num_threads',
+                        help='How many threads minion will run to process depot entries',
+                        default=4)
     return parser
 
 
-if __name__ == "__main__":
-    args = minion_argparser().parse_args()
-
-    while True:
+def process_work(worker_id,
+                 depot_host,
+                 depot_port,
+                 depot_queue_id,
+                 sleep_time)
+    running = True
+    found_work = False
+    logger = get_logger('esbuild_minion_{}'.format(worker_id))
+    while running:
         # Get work from depot api:
         work = requests.get('http://{}:{}/v0/work/{}'
-                            .format(args.host, args.port, args.queue_id))
+                            .format(depot_host, depot_port, depot_queue_id))
         try:
             work = work.json()
         except Exception as err:
             logger.error("Invalid job: {}\nError: {}".format(work, err))
-
-        if work.get('status') == 'No work found':
-            continue
-
-        try:
-            # Make sure that arguments are valid:
-            esbuild_argparser().parse_args(work['arguments'])
-            # Compose and execute the command:
-            command = ('sudo /var/tungsten/services/esbuild/es_build_{}_wrapper {}'
-                       .format(work['build_type'], ' '.join(work['arguments'])))
-            logger.info('-> Running {}'.format(command))
-            if args.do_not_wait_for_completion:
-                # NOTE: Will result in all jobs of the queue running on a single machine
-                subprocess.Popen(command, shell=True)
+        else:
+            if work.get('status') == 'No work found':
+                if found_work:
+                    running = False
             else:
-                subprocess.call(command, shell=True)
-        except Exception as err:
-            logger.error("Attempted to run job: {}\nError: {}".format(work, repr(err)))
+                try:
+                    # Make sure that arguments are valid:
+                    esbuild_argparser().parse_args(work['arguments'])
+                    # Compose and execute the command:
+                    command = ('sudo /var/tungsten/services/esbuild/es_build_{}_wrapper {}'
+                               .format(work['build_type'], ' '.join(work['arguments'])))
+                    logger.info('-> Running {}'.format(command))
+                    subprocess.call(command, shell=True)
+                except Exception as err:
+                    logger.error("Attempted to run job: {}\nError: {}".format(work, repr(err)))
 
-        time.sleep(TIMEDELTA)
+            time.sleep(sleep_time)
+
+if __name__ == "__main__":
+    args = minion_argparser().parse_args()
+    
+    threads = []
+
+    # create processes
+    for i in range(0, num_threads):
+        logger.info("Creating thread {}".format(i))
+        thread_info = {}
+
+        thread_info['id'] = i
+        thread_info['process'] = Process(target=process_work,
+                                         args=(i,
+                                               args.host,
+                                               args.port,
+                                               args.queue_id,
+                                               TIMEDELTA)
+        thread_info['status'] = "running"
+        thread_count = thread_count + 1
+        threads.append(thread_info)
+        thread_info['process'].start()
+
+    for thread in threads:
+        thread['process'].join()
+
