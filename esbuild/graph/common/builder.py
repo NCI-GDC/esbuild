af26de4020ed19ef6ce11ee62ea48a7a331cd7d4
Max Popov <maxpmy@gmail.com>
2018-12-12 15:27:48 -0600

    feat(add):[DAT-1964] baseid field

--- esbuild/graph/common/builder.py
+++ esbuild/graph/common/builder.py
@@ -127,7 +127,7 @@ class GraphIndexBuilder(object):
     """
 
     data_file_categories = ['data_file', 'metadata_file']
-    data_file_indexd_fields = ['acl', 'file_size', 'file_name', 'file_state', 'md5sum', 'release_number', 'version']
+    data_file_indexd_fields = ['acl', 'file_size', 'file_name', 'file_state', 'md5sum', 'release_number', 'version', 'baseid']
 
     mapper = None
 
@@ -799,8 +799,7 @@ class GraphIndexBuilder(object):
         self.add_file_associated_entities(node, doc, case_id)
         self.add_annotations(node, relevant, doc)
         self.add_acl(node, doc)
-        self.add_version(node, doc)
-        self.add_release_number(node, doc)
+        self.add_versioning_fields(node, doc)
         self.add_file_data_format(node, doc)
 
         return doc
@@ -1167,6 +1166,13 @@ class GraphIndexBuilder(object):
         if annotations:
             doc['annotations'] = annotations
 
+    def add_versioning_fields(self, node, doc):
+        """Add the versioning fields to the file document.
+
+        """
+        for key in ['release_number', 'version', 'baseid']:
+            doc[key] = getattr(node, key)
+
     def add_acl(self, node, doc):
         """Add the protection status of a file to the file document.
 
@@ -1174,18 +1180,6 @@ class GraphIndexBuilder(object):
 
         self.add_file_access(node, doc)
         doc['acl'] = node.acl
-
-    def add_version(self, node, doc):
-        """Add the version of a file to the file document.
-
-        """
-        doc['version'] = node.version
-
-    def add_release_number(self, node, doc):
-        """Add release_number of a file to the file document.
-
-        """
-        doc['release_number'] = node.release_number
 
     def add_file_access(self, node, doc):
         """Summarizes whether the ACL implies that the file is either ``open``
