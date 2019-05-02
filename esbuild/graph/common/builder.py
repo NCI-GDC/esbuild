d0336381582e37e198950f9c50db6c89e67cb030
Joe Sislow <madopal@uchicago.edu>
2019-05-02 22:29:58 +0000

    feat(esbuild): Add remove-input-files change from TT-928
    - Add remove-input-files change merged earlier from Maxim's
    change for TT-928

--- esbuild/graph/common/builder.py
+++ esbuild/graph/common/builder.py
@@ -601,7 +601,12 @@ class GraphIndexBuilder(object):
         case['files'] = [{k: f[k] for k in f if k not in ['cases',
                                                           'annotations',
                                                           'associated_entities']}
-                         for f in returned_files]
+                         for f in deepcopy(returned_files)]
+
+        # Do not include input_files in case.files.analysis (TT-928)
+        for f in case['files']:
+            if 'analysis' in f:
+                f['analysis'].pop('input_files', None)
 
         self.validate_case(node, case)
 
