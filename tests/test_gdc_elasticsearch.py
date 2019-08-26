752b432ef01b8eb516d64da063aacef8ab33d913
Patrick Huarng <huarngpa@gmail.com>
2019-08-26 10:59:44 -0500

    fix(pr): add a test case for released annotation

--- tests/test_gdc_elasticsearch.py
+++ tests/test_gdc_elasticsearch.py
@@ -84,6 +84,11 @@ def test_basic_es_generate(setup_test, init_indexd, converter):
             doc_type='annotation',
             id=get_node_id('block-release-annotation'),
         )
+        assert not es.exists(
+            index='gdc_es_test',
+            doc_type='annotation',
+            id=get_node_id('block-release-annotation-released'),
+        )
         assert es.exists( # just checking
             index='gdc_es_test',
             doc_type='annotation',
