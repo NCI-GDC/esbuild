c97b4e0753bfb28d2966605991ae3ed5199d0fe7
Patrick Huarng <huarngpa@gmail.com>
2019-08-26 09:20:59 -0500

    chore(tests): add some tests

--- tests/data.py
+++ tests/data.py
@@ -649,6 +649,15 @@ NODES = [
         primary_site='Rectum',
         disease_type='Rectum Adenocarcinoma'
     ),
+    Case(
+        # TT-1044 blocking release
+        node_id=get_node_id('blocking-release case'),
+        project_id='TCGA-BRCA',
+        state='submitted',
+        submitter_id='unreleased_case_submitter_2',
+        primary_site='Rectum',
+        disease_type='Rectum Adenocarcinoma'
+    ),
     Portion(
         node_id=get_node_id('portion-01'),
         project_id='TCGA-BRCA',
@@ -887,6 +896,13 @@ NODES = [
         state='released',
         submitter_id='18675',
         status="Rescinded",
+    ),
+    # TT-1044 blocking release
+    Annotation(
+        node_id=get_node_id('block-release-annotation'),
+        state='submitted',
+        status='Approved',
+        classification='Blocking Release',
     ),
     Annotation(
         node_id=get_node_id('annotation-without-downstream'),
@@ -1197,6 +1213,11 @@ EDGES = [
     AnnotationAnnotatesCase(
         src_id=get_node_id('rescinded-redaction-annotation'),
         dst_id=get_node_id('case-tcga-brca-breast'),
+    ),
+    # TT-1044 blocking release
+    AnnotationAnnotatesCase(
+        src_id=get_node_id('block-release-annotation'),
+        dst_id=get_node_id('blocking-release case'),
     ),
     AnnotationAnnotatesCase(
         src_id=get_node_id('unreleased-annotation'),
@@ -1727,6 +1748,10 @@ EDGES = [
     ),
     CaseMemberOfProject(
         src_id=get_node_id('unreleased-case-in-released-project'),
+        dst_id=get_node_id('fake_active_project')
+    ),
+    CaseMemberOfProject(
+        src_id=get_node_id('blocking-release case'),
         dst_id=get_node_id('fake_active_project')
     ),
 ]
