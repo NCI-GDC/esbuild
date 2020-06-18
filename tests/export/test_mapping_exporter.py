f93e7efd044c090ee8373ad66bde195769e9e684
Greg Singer <singergr@uchicago.edu>
2020-06-18 18:31:38 -0500

    DEV-236 actually verify exported mappings

--- tests/export/test_mapping_exporter.py
+++ tests/export/test_mapping_exporter.py
@@ -1,31 +1,56 @@
 import pathlib
 import tempfile
+from typing import Dict
+
+import yaml
 
 from esbuild.export import mapping_exporter
+from esbuild.graph.active import mappings
+
+
+def load_yaml(path: pathlib.Path) -> Dict:
+    with path.open() as in_file:
+        return yaml.safe_load(in_file)
 
 
 def verify_export(output_dir: mapping_exporter.PathName) -> None:
-    """Verify the exported mappings/settings files in a given output directory."""
+    """Verify the exported mappings/settings files in the given output directory."""
     output_path = pathlib.Path(output_dir)
 
-    # TODO Actually check file contents. Just do a smoke test right now.
-    exported_filenames = set(p.name for p in output_path.iterdir())
-    assert exported_filenames == {
-        "annotation.mapping.yaml",
-        "case.mapping.yaml",
-        "descriptions.yaml",
-        "file.mapping.yaml",
-        "project.mapping.yaml",
-        "settings.yaml",
-    }
+    assert (
+        load_yaml(output_path / "annotation.mapping.yaml")["properties"]
+        == mappings.ActiveESMapper.get_annotation_es_mapping()["properties"]
+    )
+    assert (
+        load_yaml(output_path / "case.mapping.yaml")["properties"]
+        == mappings.ActiveESMapper.get_case_es_mapping()["properties"]
+    )
+    assert (
+        load_yaml(output_path / "file.mapping.yaml")["properties"]
+        == mappings.ActiveESMapper.get_file_es_mapping()["properties"]
+    )
+    assert (
+        load_yaml(output_path / "project.mapping.yaml")["properties"]
+        == mappings.ActiveESMapper.get_project_es_mapping()["properties"]
+    )
+
+    assert (
+        load_yaml(output_path / "descriptions.yaml")["_meta"]["descriptions"]
+        == mappings.ActiveESMapper.get_descriptions()
+    )
+
+    assert (
+        load_yaml(output_path / "settings.yaml")
+        == mappings.ActiveESMapper.index_settings()["settings"]
+    )
 
 
 def test_export():
     """Export the graph mappings and verify the written files match the actual mappings.
 
     This test is kind of tautological in that it basically just checks if we wrote
-    some files, but there isn't a lot else to test short of creating ES indices and
-    seeing if the mappings match what go into ES.
+    some files, but there isn't much else we can test without digging into the mapper
+    logic, which is better tested elsewhere.
     """
     exporter = mapping_exporter.MappingExporter()
     with tempfile.TemporaryDirectory() as tempdir:
