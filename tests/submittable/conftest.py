aa6946df51d0e6d1739cffdf9dfd423ef6353abf
Maksim An <anmaxvl@gmail.com>
2020-07-15 13:39:23 -0500

    DEV-269: Enable indexing of pathology_report nodes

---
+++ tests/submittable/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+from esbuild.graph.active.builder import ActiveGraphIndexBuilder
+from tests.conftest import Index
+
+
+@pytest.fixture
+def pathology_report_graph(generate_scenario):
+    generate_scenario("pathology_report_scenario.yaml")
+
+
+@pytest.fixture
+def pathology_index(pg_driver, init_indexd, pathology_report_graph):
+    builder = ActiveGraphIndexBuilder(pg_driver, init_indexd)
+    with pg_driver.session_scope():
+        builder.cache_database()
+    index = builder.denormalize_all()
+
+    return Index._make(index)
