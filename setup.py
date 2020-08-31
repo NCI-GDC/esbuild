184885f5bf41cf179130352307cdb533407391bd
Maksim An <anmaxvl@gmail.com>
2020-08-31 10:08:04 -0500

    downgrade setuptools

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setup(
         "progressbar2>=2.0,<4",
         "python-dotenv~=0.10.3",
         "requests~=2.7",
+        "setuptools!=50",
         "six~=1.12",
         "SQLAlchemy==1.3.3",
         "psqlgraph @ git+ssh://git@github.com/NCI-GDC/psqlgraph.git@3.0.0#egg=psqlgraph",
