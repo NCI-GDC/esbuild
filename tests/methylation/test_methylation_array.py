996001857703d840d690ee03bbdd9853ea5c03dc
qiaouchicago <qiaoq@uchicago.edu>
2021-04-28 15:13:17 -0500

    update gdcdictionary

--- tests/methylation/test_methylation_array.py
+++ tests/methylation/test_methylation_array.py
@@ -6,23 +6,23 @@ from jsonpath_rw import parse
 
 @pytest.mark.parametrize("index_type, path, expectations", [
     ("files", "[*].type.[*]", {
+        "masked_methylation_array": 4,
         "methylation_beta_value": 3,  # 1 from data.py 2 from methylation_array_scenario.yaml
         "raw_methylation_array": 3,
-        "masked_methylation_array": 4,
     }),
     ("files", "[*].data_type.[*]", {
+        "Masked Intensities": 4,
         "Methylation Beta Value": 3,
         "Raw Intensities": 3,
-        "Masked Intensities": 4,
     }),
     ("files", "[*].submitter_id.[*]", {
         'mbv_0': 1,
         'mbv_1': 1,
     }),
     ('cases', '[*].files.[*].data_type.[*]', {
+        "Masked Intensities": 4,
         'Methylation Beta Value': 3,
         "Raw Intensities": 3,
-        "Masked Intensities": 4,
     }),
 ])
 def test_methylation_array_counts(methylation_index, index_type, path, expectations):
