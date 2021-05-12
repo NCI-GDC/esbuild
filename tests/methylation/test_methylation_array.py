61a177953427929cd009a025303a458080e923a2
qiaouchicago <qiaoq@uchicago.edu>
2021-05-12 15:12:40 -0500

    add channel for raw methylation array

--- tests/methylation/test_methylation_array.py
+++ tests/methylation/test_methylation_array.py
@@ -20,7 +20,7 @@ from jsonpath_rw import parse
         'mbv_1': 1,
     }),
     ("files", "[*].channel.[*]", {
-        'Red': 2,
+        'Red': 5,   # 3 raw_methylation_array + 2 masked_methylation_array
         'Green': 2,
     }),
     ('cases', '[*].files.[*].data_type.[*]', {
