745ece47873c4a6d023f99337473d43a05622580
Qiao Qiao <qiaoq@uchicago.edu>
2022-05-25 11:14:06 -0500

    update docstring for esbuild graph common validators py

--- esbuild/graph/common/validators.py
+++ esbuild/graph/common/validators.py
@@ -1,9 +1,10 @@
 def is_node_hidden(node):
-    """Return True if the node should be traversed (and therefore must
+    """Check whether node should be traversed but should not in any documents.
+
+    Return True if the node should be traversed (and therefore must
     remain in the cache) but should not appear in any documents
 
     """
-
     # Hide all submitted_* node types from indices
     return node.label.startswith("submitted_") or node.label in [
         "archive",
