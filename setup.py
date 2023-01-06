56d95d34e2ffc6d507b4f5937de3ba0befa5e164
Qiao Qiao <qiaoq@uchicago.edu>
2023-01-06 09:18:44 -0600

    update pin for indexclient

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         "gdcdictionary @ git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
         "gdcdatamodel @ git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
         "gdc_ng_models @ git+ssh://git@github.com/NCI-GDC/gdc-ng-models.git@1.5.2#egg=gdc_ng_models",
-        "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@a920a5c68bd908f295557b87d5cfe44f378cab95#egg=indexclient",
+        "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.0-rc.1#egg=indexclient",
         "queueclient @ git+ssh://git@github.com/NCI-GDC/queueclient.git@1.2.0#egg=queueclient",
         "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@2.8.1-rc.2#egg=gdcmodels",
         "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@2.0.4#egg=normalizer",
