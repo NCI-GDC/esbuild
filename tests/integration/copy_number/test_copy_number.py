58eed7bbadc718fb2d3a968aea61fc264652ef52
Michael Byrnes <byrnesmi@uchicago.edu>
2023-05-19 12:53:51 -0500

    DEV-1549: Fix tests.

--- tests/integration/copy_number/test_copy_number.py
+++ tests/integration/copy_number/test_copy_number.py
@@ -1,5 +1,4 @@
-import jsonpath_ng
-from jsonpath_ng import ext
+import jmespath
 
 from tests.integration import conftest
 
@@ -8,10 +7,9 @@ def test_copy_number_estimate(copy_number_estimate_index: conftest.Index) -> Non
     """
     Make sure that CopyNumberSegment and CopyNumberEstimate nodes are picked up
     """
-    file_results = jsonpath_ng.parse("[*].submitter_id").find(
-        copy_number_estimate_index.files
+    file_submitter_ids = frozenset(
+        jmespath.search("[].submitter_id", copy_number_estimate_index.files)
     )
-    file_submitter_ids = frozenset(r.value for r in file_results)
 
     # 8 additional files: 2 CNE, 2 CNS, 2 ARs, 2 SGA
     expected_submitter_ids = frozenset(
@@ -33,24 +31,21 @@ def test_copy_number_estimate(copy_number_estimate_index: conftest.Index) -> Non
 
 
 def test_copy_number_segment(copy_number_segment_index: conftest.Index) -> None:
-    file_results = jsonpath_ng.parse("[*].submitter_id").find(
-        copy_number_segment_index.files
+    file_ids = frozenset(
+        jmespath.search("[].submitter_id", copy_number_segment_index.files)
     )
-    file_ids = frozenset(r.value for r in file_results)
     expected_ids = frozenset(("gta_cns_1", "tcn_cns_1"))
 
     assert expected_ids < file_ids, expected_ids - file_ids
 
-    genotyping_results = ext.parse(
-        "$[?submitter_id=gta_cns_1].associated_entities.[*].entity_submitter_id"
-    ).find(copy_number_segment_index.files)
-    tangent_copy_number_results = ext.parse(
-        "$[?submitter_id=tcn_cns_1].associated_entities.[*].entity_submitter_id"
-    ).find(copy_number_segment_index.files)
+    genotyping_results = jmespath.search(
+        "[?submitter_id==`gta_cns_1`].associated_entities[].entity_submitter_id",
+        copy_number_segment_index.files,
+    )
+    tangent_copy_number_results = jmespath.search(
+        "[?submitter_id==`tcn_cns_1`].associated_entities[].entity_submitter_id",
+        copy_number_segment_index.files,
+    )
 
-    assert frozenset(("gta_aliquot_1",)) == frozenset(
-        r.value for r in genotyping_results
-    )
-    assert frozenset(("tcn_aliquot_1",)) == frozenset(
-        r.value for r in tangent_copy_number_results
-    )
+    assert frozenset(("gta_aliquot_1",)) == frozenset(genotyping_results)
+    assert frozenset(("tcn_aliquot_1",)) == frozenset(tangent_copy_number_results)
