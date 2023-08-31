377c6e2a97e3cf0d4d7c81da9f7fc6d9831ac9e7
Qiao Qiao <qiaoq@uchicago.edu>
2023-08-31 16:12:33 -0500

    update deps

--- setup.py
+++ setup.py
@@ -33,14 +33,14 @@ setup(
         "python-dotenv~=0.10.3",
         "requests~=2.7",
         "SQLAlchemy==1.3.3",
-        "psqlgraph @ git+ssh://git@github.com/NCI-GDC/psqlgraph.git@3.3.0#egg=psqlgraph",
+        "psqlgraph @ git+ssh://git@github.com/NCI-GDC/psqlgraph.git@4.0.2#egg=psqlgraph",
         "gdcdictionary @ git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
-        "gdcdatamodel @ git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
+        "gdcdatamodel @ git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@4.1.1#egg=gdcdatamodel",
         "gdc_ng_models @ git+ssh://git@github.com/NCI-GDC/gdc-ng-models.git@1.5.2#egg=gdc_ng_models",
-        "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.3#egg=indexclient",
+        "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.7#egg=indexclient",
         "queueclient @ git+ssh://git@github.com/NCI-GDC/queueclient.git@1.2.0#egg=queueclient",
-        "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@4.0.0-rc.1#egg=gdcmodels",
-        "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@4.0.0-rc.1#egg=normalizer",
+        "gdcmodels @ git+https://github.com/NCI-GDC/gdc-models.git@4.0.0#egg=gdcmodels",
+        "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@4.0.0#egg=normalizer",
     ],
     extras_require={
         "dev": [
@@ -54,7 +54,7 @@ setup(
             "pytest-postgresql",  # this should not be needed
             "indexdmodels @ git+ssh://git@github.com/NCI-GDC/indexdmodels.git@0.2.0#egg=indexdmodels",  # this should not be needed
             "indexd @ git+ssh://git@github.com/NCI-GDC/indexd.git@2.13.0#egg=indexd",
-            "indexclient[pytest_indexd] @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.3#egg=indexclient[pytest_indexd]",
+            "indexclient[pytest_indexd] @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.7#egg=indexclient[pytest_indexd]",
         ]
     },
     scripts=[
