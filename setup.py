a82343c8b452d5155f60b4c1648483f236ce9d7d
Gitlab CI <gitlab@git.osdc.io>
2023-08-31 20:55:45 +0000

    Update indexclient to 2.3.7 180564d9c60a77c54f7ba206578fd49ee60ac653

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         "gdcdictionary @ git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
         "gdcdatamodel @ git+ssh://git@github.com/NCI-GDC/gdcdatamodel.git@3.4.0#egg=gdcdatamodel",
         "gdc_ng_models @ git+ssh://git@github.com/NCI-GDC/gdc-ng-models.git@1.5.2#egg=gdc_ng_models",
-        "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.3#egg=indexclient",
+        "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.7#egg=indexclient",
         "queueclient @ git+ssh://git@github.com/NCI-GDC/queueclient.git@1.2.0#egg=queueclient",
         "gdcmodels @ git+ssh://git@github.com/NCI-GDC/gdc-models.git@4.0.0-rc.1#egg=gdcmodels",
         "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@4.0.0-rc.1#egg=normalizer",
@@ -57,7 +57,7 @@ setup(
             "pytest-elasticsearch",
             "pytest-postgresql",  # this should not be needed
             "indexd @ git+ssh://git@github.com/NCI-GDC/indexd.git@2.13.0#egg=indexd",
-            "indexclient[pytest_indexd] @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.3#egg=indexclient[pytest_indexd]",
+            "indexclient[pytest_indexd] @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.3.7#egg=indexclient[pytest_indexd]",
         ]
     },
     scripts=[
