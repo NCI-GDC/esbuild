0de9f758bf29140514c85416008bd3461ddc4fa9
ProfOak <OpenProfOak@gmail.com>
2023-10-09 16:30:02 -0500

    DEV-2143: bump gdcmodels pin
    This pin has to have the models generated by the esbuild-cli command or
    the tests will fail.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "gdc_ng_models @ git+ssh://git@github.com/NCI-GDC/gdc-ng-models.git@1.5.2#egg=gdc_ng_models",
         "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.4.0#egg=indexclient",
         "queueclient @ git+ssh://git@github.com/NCI-GDC/queueclient.git@1.2.0#egg=queueclient",
-        "gdcmodels @ git+https://github.com/NCI-GDC/gdc-models.git@4.1.0#egg=gdcmodels",
+        "gdcmodels @ git+https://github.com/NCI-GDC/gdc-models.git@feat/DEV-2143-generate-obsolete-mappings#egg=gdcmodels",
         "normalizer @ git+ssh://git@github.com/NCI-GDC/normalizer.git@develop#egg=normalizer",
     ],
     extras_require={
