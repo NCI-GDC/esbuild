4003bd91fcf31f95124b9b961e415e636bbea5c8
Qiao Qiao <qiaoq@uchicago.edu>
2023-10-27 11:22:23 -0500

    update gdc_ng_models

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "psqlgraph @ git+https://github.com/NCI-GDC/psqlgraph.git@3.4.0#egg=psqlgraph",
         "gdcdictionary @ git+ssh://git@github.com/NCI-GDC/gdcdictionary.git@2.4.0#egg=gdcdictionary",
         "gdcdatamodel2 @ git+ssh://git@github.com/NCI-GDC/gdcdatamodel2.git@2.4.0#egg=gdcdatamodel2",
-        "gdc_ng_models @ git+ssh://git@github.com/NCI-GDC/gdc-ng-models.git@1.5.2#egg=gdc_ng_models",
+        "gdc_ng_models @ git+ssh://git@github.com/NCI-GDC/gdc-ng-models.git@1.6.3#egg=gdc_ng_models",
         "indexclient @ git+ssh://git@github.com/NCI-GDC/indexclient.git@2.4.0#egg=indexclient",
         "queueclient @ git+ssh://git@github.com/NCI-GDC/queueclient.git@1.2.0#egg=queueclient",
         "gdcmodels @ git+https://github.com/NCI-GDC/gdc-models.git@4.0.0#egg=gdcmodels",
