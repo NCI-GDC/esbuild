7dc216f046e5c85a7bdfd2a0807089eb49489a85
Qiao Qiao <qiaoq@uchicago.edu>
2023-10-30 09:38:29 -0500

    update case node state

--- tests/integration/data.py
+++ tests/integration/data.py
@@ -591,10 +591,10 @@ NODES = [
         disease_type="Miscellaneous Bone Tumors",
     ),
     models.Case(
-        # processed case in AWG project
+        # validated case in AWG project
         node_id=get_node_id("processed-awg-case"),
         project_id="INTERNAL-AWG-ONE",
-        state="processed",
+        state="validated",
         submitter_id="INTERNAL-AWG-ONE-CASE-0002",
         primary_site="Bones, joints and articular cartilage of limbs",
         disease_type="Miscellaneous Bone Tumors",
